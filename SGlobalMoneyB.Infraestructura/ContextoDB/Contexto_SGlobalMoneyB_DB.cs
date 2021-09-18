eadc9e039c82cd08cfc678a072a9a1a170e39e45
Ziel <frans_421_12@hotmail.com>
2021-09-17 22:40:24 -0500

    feat: Se agrego el Contexto y Inicializador

---
+++ SGlobalMoneyB.Infraestructura/ContextoDB/Contexto_SGlobalMoneyB_DB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGlobalMoneyB.Infraestructura.ContextoDB
+{
+    public class Contexto_SGlobalMoneyB_DB : DbContext
+    {
+        public Contexto_SGlobalMoneyB_DB() : base("name=Contexto_SGlobalMoneyB_DB_Con") { }
+        // llamando las entidades para crear las tablas
+    }
+}
---
+++ SGlobalMoneyB.Infraestructura/Inicializador/SGlobalMoneyB_Inicializador.cs
@@ -0,0 +1,18 @@
+﻿using SGlobalMoneyB.Infraestructura.ContextoDB;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGlobalMoneyB.Infraestructura.Inicializador
+{
+    class SGlobalMoneyB_Inicializador : DropCreateDatabaseAlways<Contexto_SGlobalMoneyB_DB>
+    {
+        protected override void Seed(Contexto_SGlobalMoneyB_DB context)
+        {
+            base.Seed(context);
+        }
+    }
+}
